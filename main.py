906d5f527fc91ebe52862e278fabeebff357c31c
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-02-22 15:32:03 +0300

    main and save to scsv functions

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import os
 import time
 from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options = options)
+driver = webdriver.Chrome(options=options)
 url = "https://www.betika.com/en-ke/live"
 driver.get(url)
 driver.implicitly_wait(10)
+
 
 
 class Match:
@@ -50,54 +53,68 @@ def filter_for_ongoing():
     apply.click()
 
 
-login_details("0115361123", "Kereskwe1")
-filter_for_ongoing()
+def save_csv_to_file(arr, file_name):
+    arr = pd.DataFrame(arr)
+    csv_file_path = f'C:\\Users\\Techron\\PycharmProjects\\AeroBet\{file_name}'
 
-live_matches = driver.find_elements(By.XPATH, "//div[@class='live-match']")
-
-#Match data collection
-match_array = []
-for i, match in enumerate(live_matches):
-    teams = match.find_element(By.XPATH, ".//div[@class='live-match__teams']")
-    # Separate the two teams and store the teams and scores
-    hteam = teams.text.split("\n")[0]
-
-    # Find the index where letters start for hteam
-    index_of_first_letter = next((i for i, char in enumerate(hteam) if not char.isdigit()), len(hteam))
-    hteam_scores = hteam[:index_of_first_letter]
-    hteam_labels = hteam[index_of_first_letter:]
-
-    ateam = teams.text.split("\n")[1]
-    index_of_first_letter = next((i for i, char in enumerate(ateam) if not char.isdigit()), len(ateam))
-    ateam_scores = ateam[:index_of_first_letter]
-    ateam_labels = ateam[index_of_first_letter:]
-
-    # print(f"Home team: {hteam_labels}, Home team scores: {hteam_scores}")
-    # print(f"Away team: {ateam_labels}, Away team scores: {ateam_scores}")
-
-    # separate the odds
-    odds = match.find_element(By.XPATH, ".//div[@class='live-match__odds__container']")
-    odds = odds.text
-    if odds.count("\n") == 2:
-        oddlist = odds.split("\n")
-        odds_1 = float(oddlist[0]) if oddlist[0] != "-" else float('inf')
-        odds_x = float(oddlist[1]) if oddlist[1] != "-" else float('inf')
-        odds_2 = float(oddlist[2]) if oddlist[2] != "-" else float('inf')
+    if not os.path.exists(csv_file_path):
+        arr.to_csv(csv_file_path)
     else:
-        oddlist = odds.split("\n")
-        odds_1 = float(oddlist[0]) if oddlist[0] != "-" else float('inf')
-        odds_x = float('inf')
-        odds_2 = float(oddlist[1]) if oddlist[1] != "-" else float('inf')
+        arr.to_csv(csv_file_path, mode='a', header=False) #append to the end
 
-    match_object = Match(hteam_labels, ateam_labels, hteam_scores, ateam_scores, odds_1, odds_x, odds_2)
-    match_array.append(match_object)
-
-print(f"Total Matches: {len(live_matches)}")
-
-for match in match_array:
-    if match.odd_1<1.3 or match.odd_2<1.3 or match.odd_x<1.2:
-        print(match.home_team,match.odd_1,match.odd_x,match.odd_2)
+    print("Saved to csv file")
 
 
-time.sleep(5)
-driver.quit()
+def main():
+    login_details("0115361123", "Kereskwe1")
+    filter_for_ongoing()
+
+    live_matches = driver.find_elements(By.XPATH, "//div[@class='live-match']")
+
+    #Match data collection
+    games = []
+    match_array = []
+    for i, match in enumerate(live_matches):
+        teams = match.find_element(By.XPATH, ".//div[@class='live-match__teams']")
+        # Separate the two teams and store the teams and scores
+        hteam = teams.text.split("\n")[0]
+
+        # Find the index where letters start for hteam
+        index_of_first_letter = next((i for i, char in enumerate(hteam) if not char.isdigit()), len(hteam))
+        hteam_scores = hteam[:index_of_first_letter]
+        hteam_labels = hteam[index_of_first_letter:]
+
+        ateam = teams.text.split("\n")[1]
+        index_of_first_letter = next((i for i, char in enumerate(ateam) if not char.isdigit()), len(ateam))
+        ateam_scores = ateam[:index_of_first_letter]
+        ateam_labels = ateam[index_of_first_letter:]
+
+        # separate the odds
+        odds = match.find_element(By.XPATH, ".//div[@class='live-match__odds__container']")
+           #obtain the individual divs so that we can use the buttons for odd placing
+        if odds.text.count("\n") == 2:
+            oddlist = odds.text.split("\n")
+            odds_1 = float(oddlist[0]) if oddlist[0] != "-" else float('inf')
+            odds_x = float(oddlist[1]) if oddlist[1] != "-" else float('inf')
+            odds_2 = float(oddlist[2]) if oddlist[2] != "-" else float('inf')
+        else:
+            oddlist = odds.text.split("\n")
+            odds_1 = float(oddlist[0]) if oddlist[0] != "-" else float('inf')
+            odds_x = float('inf')
+            odds_2 = float(oddlist[1]) if oddlist[1] != "-" else float('inf')
+
+        match_object = Match(hteam_labels, ateam_labels, hteam_scores, ateam_scores, odds_1, odds_x, odds_2)
+        match_array.append(match_object)
+        games.append({"Home":hteam_labels,"Away":ateam_labels,"1":odds_1,"X":odds_x,"2":odds_2,"Expected Win":})
+
+    print(f"Total Matches: {len(live_matches)}")
+
+
+    eligible = 0
+    for match in match_array:
+        if match.odd_1<1.3:# or match.odd_2<1.3 or match.odd_x<1.2:
+            print(match.home_team,match.odd_1,match.odd_x,match.odd_2)
+            eligible+=1
+    print(eligible)
+
+
